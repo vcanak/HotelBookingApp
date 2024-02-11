9a3a530a688055958e675000d720b872c202833f
Vedat Can AK <vcan.ak@gmail.com>
2024-02-11 23:53:50 +0300

    Model definitions

--- hotel-booking/src/main/java/com/backend/hotelbooking/model/BookedRoom.java
+++ hotel-booking/src/main/java/com/backend/hotelbooking/model/BookedRoom.java
@@ -1,5 +1,147 @@
 package com.backend.hotelbooking.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "booked_room")
 public class BookedRoom {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_in_date")
+    private LocalDateTime checkInDate;
+    @Column(name = "check_out_date")
+    private LocalDateTime checkOutDate;
+    @Column(name = "customer_full_name")
+    private String customerFullName;
+    @Column(name = "customer_emails")
+    private String customerEmail;
+    @Column(name = "num_of_adults")
+    private int numOfAdults;
+    @Column(name = "num_of_kids")
+    private int numOfKids;
+    @Column(name = "num_of_total_customer")
+    private int totalNumOfCustomer;
+    @Column(name = "booking_confirmation_code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+
+
+    public BookedRoom() {
+    }
+
+
+
+    private void getTotalNumOfCustomers(){
+        this.totalNumOfCustomer = this.numOfAdults + this.numOfKids;
+    };
+
+    public Long getBookingId() {
+        return this.bookingId;
+    }
+
+    public void setBookingId(Long bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public BookedRoom(Long bookingId, LocalDateTime checkInDate, LocalDateTime checkOutDate, String customerFullName, String customerEmail, int numOfAdults, int numOfKids, int totalNumOfCustomer, String bookingConfirmationCode, Room room) {
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.customerFullName = customerFullName;
+        this.customerEmail = customerEmail;
+        this.numOfAdults = numOfAdults;
+        this.numOfKids = numOfKids;
+        this.totalNumOfCustomer = totalNumOfCustomer;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+        this.room = room;
+    }
+
+    public LocalDateTime getCheckInDate() {
+        return this.checkInDate;
+    }
+
+    public void setCheckInDate(LocalDateTime checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDateTime getCheckOutDate() {
+        return this.checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDateTime checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public String getCustomerFullName() {
+        return this.customerFullName;
+    }
+
+    public void setCustomerFullName(String customerFullName) {
+        this.customerFullName = customerFullName;
+    }
+
+    public String getCustomerEmail() {
+        return this.customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public int getNumOfAdults() {
+        return this.numOfAdults;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        getTotalNumOfCustomers();
+    }
+
+    public int getNumOfKids() {
+        return this.numOfKids;
+    }
+
+    public void setNumOfKids(int numOfKids) {
+        this.numOfKids = numOfKids;
+        getTotalNumOfCustomers();
+    }
+
+    public int getTotalNumOfCustomer() {
+        return this.totalNumOfCustomer;
+    }
+
+    public String getBookingConfirmationCode() {
+        return this.bookingConfirmationCode;
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
+    public Room getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+
+
 }
--- hotel-booking/src/main/java/com/backend/hotelbooking/model/Room.java
+++ hotel-booking/src/main/java/com/backend/hotelbooking/model/Room.java
@@ -1,5 +1,119 @@
 package com.backend.hotelbooking.model;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "room")
 public class Room {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "room_type")
+    private String roomType;
+    @Column(name = "price")
+    private BigDecimal price;
+    @Column(name = "is_booked")
+    private boolean isBooked = false;
+    @Lob
+    private Blob photo;
+
+    @OneToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room (){
+        this.bookings = new ArrayList<>();
+    }
+
+
+    public Room(Long id, String roomType, BigDecimal price, boolean isBooked, Blob photo, List<BookedRoom> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.price = price;
+        this.isBooked = isBooked;
+        this.photo = photo;
+        this.bookings = bookings;
+    }
+
+
+    public void addBooking(BookedRoom bookedRoom){
+        if (bookings == null){
+            bookings = new ArrayList<>();
+        }
+        bookings.add(bookedRoom);
+        bookedRoom.setRoom(this);
+        isBooked = true;
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        bookedRoom.setBookingConfirmationCode(bookingCode);
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoomType() {
+        return this.roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public boolean isIsBooked() {
+        return this.isBooked;
+    }
+
+    public boolean getIsBooked() {
+        return this.isBooked;
+    }
+
+    public void setIsBooked(boolean isBooked) {
+        this.isBooked = isBooked;
+    }
+
+    public List<BookedRoom> getBookings() {
+        return this.bookings;
+    }
+
+    public void setBookings(List<BookedRoom> bookings) {
+        this.bookings = bookings;
+    }
+
+    public Blob getPhoto() {
+        return this.photo;
+    }
+
+    public void setPhoto(Blob photo) {
+        this.photo = photo;
+    }
+
 }
